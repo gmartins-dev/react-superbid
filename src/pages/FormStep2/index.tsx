9f569d4c62636429a50ef20aa134d18dcdea9f2e
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-05-28 11:44:23 -0300

    Criado component SelectOption e atualizado FormStep2

--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,5 +1,55 @@
+import { useNavigate } from 'react-router-dom';
+import { ChangeEvent, useEffect } from 'react';
 import * as C from './styles';
+import {
+  useForm,
+  FormActions,
+} from '../../contexts/FormContext';
+import { Theme } from '../../components/Theme';
+import { SelectOption } from '../../components/SelectOption';
 
 export const FormStep2 = () => {
-  return <C.Container>Passo 2</C.Container>;
+  const navigate = useNavigate();
+
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2,
+    });
+  }, []);
+
+  const handleNextStep = () => {
+    if (state.name != '' && state.certificateType != null) {
+      navigate('/step2');
+    } else {
+      alert('Preencha todos os dados.');
+    }
+  };
+
+  return (
+    <Theme>
+      <C.Container>
+        <p>Passo 2/3</p>
+        <h1>
+          Deseja se cadastrar como pessoa física ou
+          jurídica?
+        </h1>
+        <hr />
+        <SelectOption
+          title="Sou Pessoa Física (PF)"
+          description="Se você for PF escolha essa opção."
+          icon="👤"
+        />
+        <SelectOption
+          title="Sou Pessoa Jurídica (PJ)"
+          description="Se você for PF escolha essa opção."
+          icon="🏢"
+        />
+
+        <button onClick={handleNextStep}>Próximo</button>
+      </C.Container>
+    </Theme>
+  );
 };
--- src/pages/FormStep2/styles.ts
+++ src/pages/FormStep2/styles.ts
@@ -1,3 +1,47 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  p {
+    font-size: 13px;
+    color: #66696f;
+  }
+  h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 26px;
+  }
+  hr {
+    height: 1px;
+    border: 0;
+    background-color: #0050a0;
+    margin: 30px 0;
+  }
+  label {
+    font-size: 13px;
+    input {
+      display: block;
+      margin-top: 7px;
+      box-sizing: border-box;
+      width: 100%;
+      padding: 20px 10px;
+      border: 2px solid #ec660b;
+      border-radius: 10px;
+      color: #131415;
+      outline: 0;
+      font-size: 15px;
+      background-color: #d6eaf5;
+    }
+  }
+  button {
+    background-color: #ec660b;
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: bold;
+    padding: 20px 40px;
+    margin-top: 30px;
+    margin-left: 80%;
+    border: 0;
+    border-radius: 30px;
+    cursor: pointer;
+  }
+`;
